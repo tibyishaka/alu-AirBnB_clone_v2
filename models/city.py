1843cd90db802da923be15f6ddb87d97d0d9f458
tibyishaka <t.ibyishaka@alustudent.com>
2025-05-25 08:58:36 +0200

    corrected some

--- models/city.py
+++ models/city.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-""" The city class, contains state ID and name """
+    """ The city class, contains state ID and name """
 __tablename__ = "cities"
 name = Column(String(128), nullable=False)
 places = relationship('Place', backref='cities', cascade="delete")
