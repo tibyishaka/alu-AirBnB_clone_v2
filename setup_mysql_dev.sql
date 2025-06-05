3c2feffca537c9da5ae57c1fd6678c127d45130e
tibyishaka <t.ibyishaka@alustudent.com>
2025-06-05 18:05:10 +0200

    done 8

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,15 @@
--- create database hbnb_dev_db
+-- Create database hbnb_dev_db if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- cretae user hbnb_dev (in localhost) with password hbnb_dev_pwd
+
+-- Create user 'hbnb_dev'@'localhost' with password 'hbnb_dev_pwd' if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- hbnb_dev has all privileges on only hbnb_dev_db
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- hbnb_dev has SELECT privilege on the database performance_schema
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- If hbnb_dev_db or user hbnb_dev already exists, script does not fail
+
+-- Apply the changes made to privileges
+FLUSH PRIVILEGES;
+
